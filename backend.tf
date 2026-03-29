6cdb05bc9ec54334dcc6c942cb97836b6e8564e3
zerotohero200222 <aebharath17@gmail.com>
2026-03-29 22:25:45 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "gcs" {
+    bucket  = "eighth-physics-489004-b2-tf-state"
+    prefix  = "cloudrun-restart"
+  }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
