23939818c484937f652370c390d82c924ae3c298
zerotohero200222 <aebharath17@gmail.com>
2026-03-30 20:11:09 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,11 +3,4 @@ terraform {
     bucket  = "eighth-physics-489004-b2-tf-state"
     prefix  = "cloudrun-restart"
   }
-
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-  }
 }
