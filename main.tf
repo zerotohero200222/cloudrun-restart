9faf7e06a8fdaeff02b8ae8a2b72d80a9eedec70
zerotohero200222 <aebharath17@gmail.com>
2026-03-31 21:39:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,26 @@
-# Enable APIs
+# Enable required APIs (safe guard)
 resource "google_project_service" "apis" {
   for_each = toset([
-    "run.googleapis.com",
-    "cloudbuild.googleapis.com",
-    "artifactregistry.googleapis.com",
-    "cloudresourcemanager.googleapis.com"
+    "run.googleapis.com"
   ])
+
   project = var.project_id
   service = each.key
 }
 
-# Restart Cloud Run using gcloud
+# Restart Cloud Run (NO IMAGE USED)
 resource "null_resource" "restart_cloud_run" {
 
   provisioner "local-exec" {
     command = <<EOT
+      echo "Restarting Cloud Run: ${var.service_name}"
+
       gcloud run services update ${var.service_name} \
         --region=${var.region} \
-        --update-env-vars=RESTART_TRIGGER=$(date +%s) \
-        --project=${var.project_id}
+        --project=${var.project_id} \
+        --update-env-vars=RESTART_TRIGGER=$(date +%s)
+
+      echo "Restart completed"
     EOT
   }
 
