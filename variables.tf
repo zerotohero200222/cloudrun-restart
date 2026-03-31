ecd48a9a0f7637d55accb9b2058d8f1f95884b3a
zerotohero200222 <aebharath17@gmail.com>
2026-03-31 21:38:52 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,14 @@
 variable "project_id" {
+  type    = string
   default = "eighth-physics-489004-b2"
 }
 
 variable "region" {
+  type    = string
   default = "us-central1"
 }
 
 variable "service_name" {
+  type    = string
   default = "hello"
 }
