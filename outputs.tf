c620316ead4834394fd04b1e9b5174efc9be13f9
zerotohero200222 <aebharath17@gmail.com>
2026-04-08 07:54:09 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,14 @@
-output "restart_status" {
-  value = "Cloud Run restart executed successfully"
+output "restarted_service" {
+  description = "Service that was redeployed"
+  value       = google_cloud_run_service.service.name
+}
+
+output "new_revision" {
+  description = "New revision created by this restart"
+  value       = google_cloud_run_service.service.status[0].latest_ready_revision_name
+}
+
+output "service_url" {
+  description = "Live URL of the restarted service"
+  value       = google_cloud_run_service.service.status[0].url
 }
